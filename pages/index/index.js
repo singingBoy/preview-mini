b2bc171156e7be32eb662e5d6ee4b70e483b001f
wangdesheng <737708900@qq.com>
2019-01-24 01:01:24 +0800

    2019年1月24日 01:01:22

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,11 @@
 const app = getApp();
 const banners = [{
   id: 1,
-  image: 'https://img95.699pic.com/photo/50022/7439.jpg_wh300.jpg',
+  image: 'http://img1.juimg.com/170620/355855-1F6200K42320.jpg',
   time: '2019年1月22日 15:57:25'
 },{
   id: 2,
-  image: 'http://img95.699pic.com/photo/50068/1411.jpg_wh860.jpg',
+  image: 'http://img4.imgtn.bdimg.com/it/u=2041099924,1985582387&fm=214&gp=0.jpg',
   time: '2019年1月23日 16:17:25'
 }];
 
