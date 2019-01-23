b71de00b998064043a1a6fd65f4c979605a399ab
singingBoy <737708900@qq.com>
2019-01-23 18:28:48 +0800

    2019年1月23日 18:28:46

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,55 +1,25 @@
-//index.js
-//获取应用实例
-const app = getApp()
+const app = getApp();
+const banners = [{
+  id: 1,
+  image: 'https://img95.699pic.com/photo/50022/7439.jpg_wh300.jpg',
+  time: '2019年1月22日 15:57:25'
+},{
+  id: 2,
+  image: 'http://img95.699pic.com/photo/50068/1411.jpg_wh860.jpg',
+  time: '2019年1月23日 16:17:25'
+}];
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    banners,
+    bannerIndex: 0,
   },
   onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  changeBanner(e) {
+    const { current } = e.detail;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      bannerIndex: current,
     })
   }
-
-})
+});
