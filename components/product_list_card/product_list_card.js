e9a56e57207802ff01b1a59f4770144793b1a2d1
singingBoy <737708900@qq.com>
2019-01-24 15:56:57 +0800

    feat:添加5种商品卡片

--- components/product_list_card/product_list_card.js
+++ components/product_list_card/product_list_card.js
@@ -1,19 +1,20 @@
+const CardType = {
+  floatImage: 1,
+  bannerImage: 2,
+  imageRight: 3,
+  imageLeft: 4,
+  imageList: 5,
+};
 Component({
   options: {
     multipleSlots: true, // 在组件定义时的选项中启用多slot支持
   },
-  properties: {},
+  properties: {
+    product: Object,
+  },
   data: {
-    images: [
-      'https://img11.360buyimg.com/n1/jfs/t9700/306/1436060266/681052/3c9ac812/59e09500Ndcdb8db5.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t10255/216/1395629457/558324/dc2739fe/59e094ffNa5d58ce6.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t9784/341/1408825319/556426/c39bea69/59e09506N1355a960.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t7900/289/3101499427/504495/c7817054/59e09507Nf82cf836.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t14830/40/1879053018/393853/4870c6db/5a5f10e8Na6dad590.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t16183/204/1733668430/510851/a2a142f0/5a5f10e9N74d2935f.jpg',
-      'https://img11.360buyimg.com/n1/jfs/t17944/112/130500470/596101/67f3faf/5a5f10ecNb78f9164.jpg',
-    ],
-    current: 3,
+    CardType,
+    current: 0,
   },
   methods: {
     onChange(event) {
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,10 @@
-const app = getApp();
-const banners = [{
-  id: 1,
-  image: 'http://img1.juimg.com/170620/355855-1F6200K42320.jpg',
-  time: '2019年1月22日 15:57:25'
-},{
-  id: 2,
-  image: 'http://img4.imgtn.bdimg.com/it/u=2041099924,1985582387&fm=214&gp=0.jpg',
-  time: '2019年1月23日 16:17:25'
-}];
+import {banners, productList} from 'data';
 
 Page({
   data: {
     banners,
     bannerIndex: 0,
+    productList,
   },
   onLoad() {
   },
