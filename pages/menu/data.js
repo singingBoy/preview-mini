32e65bf303120bba6bf3fd4f52cda2ac837c4dea
wangdesheng <737708900@qq.com>
2019-02-13 00:17:52 +0800

    2019年2月13日 00:17:49

---
+++ pages/menu/data.js
@@ -0,0 +1,5 @@
+export const searchKeys = [
+  '各类海鲜菜谱',
+  '教你做海鲜佳肴',
+];
+
--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -1,7 +1,19 @@
+import { searchKeys } from './data';
 
 Page({
   data: {
+    searchKeys,
   },
   onLoad: function () {
-  }
+  },
+  onKeyWordChange(e) {
+    const { current } = e.detail;
+    this.keyWord = this.data.searchKeys[current];
+  },
+  // 搜索
+  navigateToSearch() {
+    wx.navigateTo({
+      url: `/pages/search/search?keyword=${this.keyWord}`,
+    });
+  },
 });
